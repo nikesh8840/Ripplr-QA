b204462c19b532d231d0ece8f2d55dbc2678187b
Nikesh8840 <nikesh.kumar.giri@infinitelocus.com>
2025-07-24 18:18:09 +0530

    auto selection corrected

--- tests/salesman/autosubmit.spec.js
+++ tests/salesman/autosubmit.spec.js
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('test', async ({ page }) => {
   await page.goto('https://collection-staging.ripplr.in/login');
   await page.getByRole('spinbutton', { name: 'Salesman Phone Number' }).click();
-  await page.getByRole('spinbutton', { name: 'Salesman Phone Number' }).fill('9945693825');
+  await page.getByRole('spinbutton', { name: 'Salesman Phone Number' }).fill('9620146071');
   await page.getByRole('button', { name: 'Submit' }).click();
   await page.getByRole('textbox', { name: 'PIN Number' }).click();
   await page.getByRole('textbox', { name: 'PIN Number' }).fill('1234');
@@ -13,8 +13,9 @@ test('test', async ({ page }) => {
   await page.getByRole('cell', { name: '24' }).click();
   await page.getByRole('button', { name: 'Submit', exact: true }).click();
   await page.getByRole('img', { name: 'downArrow' }).click();
-  await page.locator('label').filter({ hasText: 'Auto' }).locator('span').nth(1).click();
+  await page.locator('.ant-radio').nth(1).click();
   await page.getByRole('button', { name: 'Update' }).click();
   await page.getByRole('button', { name: 'Submit Collection' }).click();
   await page.getByRole('button', { name: 'Yes' }).click();
+  await page.waitForTimeout(5000);
 });
