7cc027d73b810c9011fdc349c8832a6ed9499103
Nikesh8840 <nikesh.kumar.giri@infinitelocus.com>
2025-09-17 14:45:55 +0530

    uncomment

--- tests/adaptorupload/upload.spec.js
+++ tests/adaptorupload/upload.spec.js
@@ -2,13 +2,13 @@ const { test, expect } = require('@playwright/test');
 const { Uploadfile } = require('../../pages/Aupload.page');
 const config = require('../../config/base.config');
 
-// test('Upload GRN file', async ({ page }) => {
-//     const uploadfile = new Uploadfile(page);
+test('Upload GRN file', async ({ page }) => {
+    const uploadfile = new Uploadfile(page);
 
-//     await page.goto(config.baseURL43);
-//     const result = await uploadfile.Upload(config.credentials.username, config.credentials.password, 'Purchase Order', 'GRN');
-//     expect(result).toBeTruthy();
-// });
+    await page.goto(config.baseURL43);
+    const result = await uploadfile.Upload(config.credentials.username, config.credentials.password, 'Purchase Order', 'GRN');
+    expect(result).toBeTruthy();
+});
 
 
 
@@ -20,10 +20,10 @@ test('Upload salesOrder file', async ({ page }) => {
     expect(result).toBeTruthy();
 });
 
-// test('Upload SalesReturn file', async ({ page }) => {
-//     const uploadfile = new Uploadfile(page);
+test('Upload SalesReturn file', async ({ page }) => {
+    const uploadfile = new Uploadfile(page);
 
-//     await page.goto(config.baseURL43);
-//     const result = await uploadfile.Upload(config.credentials.username, config.credentials.password, 'Sales Return', 'salesreturn');
-//     expect(result).toBeTruthy();
-// });
+    await page.goto(config.baseURL43);
+    const result = await uploadfile.Upload(config.credentials.username, config.credentials.password, 'Sales Return', 'salesreturn');
+    expect(result).toBeTruthy();
+});
